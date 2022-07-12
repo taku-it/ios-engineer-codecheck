d5080b969a8dfec3021f2bd6a378835ba972849a
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2022-07-13 04:02:28 +0900

    RepositoryModel.swiftを削除し、Repository.swiftとUser.swiftを作成

--- iOSEngineerCodeCheck/GitHubAPI/Repository.swift
+++ iOSEngineerCodeCheck/GitHubAPI/Repository.swift
@@ -1,8 +1,8 @@
 //
-//  RepositoryModel.swift
+//  Repository.swift
 //  iOSEngineerCodeCheck
 //
-//  Created by 生田拓登 on 2022/07/07.
+//  Created by 生田拓登 on 2022/07/13.
 //  Copyright © 2022 YUMEMI Inc. All rights reserved.
 //
 
@@ -20,9 +20,5 @@ struct Repository: Codable {
     let watchersCount: Int
     let forksCount: Int
     let openIssuesCount: Int
-    let owner: Owner
-}
-
-struct Owner: Codable {
-    let avatarUrl: String
+    let owner: User
 }
