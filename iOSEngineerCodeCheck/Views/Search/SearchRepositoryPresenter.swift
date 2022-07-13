7361c68fd603a6c75e064f96d858275673d1bcec
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2022-07-13 15:08:57 +0900

    presenterでrepositoriesを保持するようにした。

--- iOSEngineerCodeCheck/Views/Search/SearchRepositoryPresenter.swift
+++ iOSEngineerCodeCheck/Views/Search/SearchRepositoryPresenter.swift
@@ -22,6 +22,7 @@ protocol SearchRepositoryPresenterOutput: AnyObject {
 }
 
 final class SearchRepositoryPresenter: SearchRepositoryPresenterInput {
+    private var repositories: [Repository] = []
     
     private weak var view: SearchRepositoryPresenterOutput?
     private var model: SearchRepositoryModelInput
@@ -32,12 +33,12 @@ final class SearchRepositoryPresenter: SearchRepositoryPresenterInput {
     }
     
     var numberOfRepo: Int {
-        return model.repositories.count
+        return repositories.count
     }
     
     func repository(forRow row: Int) -> Repository? {
-        guard row < model.repositories.count else { return nil }
-        return model.repositories[row]
+        guard row < repositories.count else { return nil }
+        return repositories[row]
     }
     
     func didSelectRow(at indexPath: IndexPath) {
